83c64fb16ce8a955dbb0aaa244f3a5199af1f237
andrewisoko <andrewisoko1998@gmail.com>
2025-12-08 15:55:22 +0000

    frontpage implementations of components.

--- src/components/assets/pages/HomePage.tsx
+++ src/components/assets/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 
 const HomePage = () => {
-  return <div className="grid grid-cols-2 gap-4" >
+  return <div className="grid grid-cols-[25%_75%] gap-4" >
     <ol className="mt-20 ml-14">
       <p>Home</p>
       <p>Cards</p>
@@ -8,11 +8,16 @@ const HomePage = () => {
       <p>Payments</p>
       <p>Direct Debit</p>
     </ol>
-    <div className="mt-10 bg-gray-300 h-9 w-20">
-        <span className="text-black-700 justify-center">Username
-        </span>
+    <div className="mt-10">
+      {/* * Username avatar*/}
+      <div className="h-1/4 w-1/4 rounded-full flex justify-between items-center px-8 hover:bg-gray-50">
+          <div className="bg-blue-500 h-12 w-12 rounded-full justify-end"></div>
+          <span className="text-black-700 font-semibold">Username</span>
+      </div>
+      <p className="flex">total balance</p>
+
     </div>
-  </div>  /** two grids,one containing logo and elements while the actual page */
+  </div>  
 };
 
 export default HomePage;
